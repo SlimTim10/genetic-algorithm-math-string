2a4bc589cb63f38fa025559d4052745f769d6ae7
SlimTim10 <slimtim10@gmail.com>
2023-07-28 18:38:08 -0400

    Finish implementing the algorithm

--- typescript/src/app.ts
+++ typescript/src/app.ts
@@ -1,6 +1,11 @@
 import {
   eqArrays,
   chunksOf,
+  buildArray,
+  sum,
+  scan,
+  zip,
+  last,
 } from './util';
 
 // This presents a linear approach to solving this problem, but real programming is rarely done in a linear fashion. I would normally create functions with gaps to be filled, going back and forth between them a lot. However, the first step of planning the types is something that I do.
@@ -126,13 +131,13 @@ const phenotype = (organism: Organism): string => {
 
 // I also want a way to get an organism's cleaned-up phenotype, which is safe to be evaluated. That means I first need a way to clean the genes of a chromosome, in accordance with the plan: remove any junk genes (along with their preceeding operators).
 const cleanChromosome = (chromosome: Chromosome): Chromosome => {
-  // Add a "plus" gene to the beginning so we can deal with pairs safely.
+  // Add a "plus" gene to the beginning so I can deal with pairs safely.
   const genes: Gene[] = [ [0, 0], ...chromosome];
   const pairs = chunksOf(2, genes);
   return pairs
     .filter(pair => pair.length === 2)
     .filter(([_, num]) => geneValue(num) !== "(junk)")
-    .reduce((acc, pair) => [...acc, ...pair])
+    .reduce((acc, pair) => [...acc, ...pair], [])
     .slice(1); // Remove the initial "plus" gene.
 };
 
@@ -148,7 +153,7 @@ const cleanPhenotype = (organism: Organism): string => {
 // I have to be careful here. For one, I need to be aware that using this evaluation method means the standard order of operations is being applied. Also, it's possible for the result to be NaN or infinity, if a division by 0 happens. I'll have to remember to handle these special cases when doing fitness evaluation.
 const evaluateMath = (mathStr: string): number => Number(eval(mathStr));
 
-// Now to evaluate the fitness of an organism. This part is tricky because I want organisms to have their fitness stored as part of their data. So, I need to evaluate the fitness of a future organism by evaluating its chromosome-to-be, relative to the target number.
+// Now to evaluate the fitness of an organism. This part is tricky because I want organisms to have their fitness stored as part of their data. So, I need to evaluate the fitness of an organism-to-be by evaluating its chromosome, relative to the target number.
 // The resulting fitness will be a number between 0 and 1, with 1 being perfect fitness.
 const evaluateFitness = (chromosome: Chromosome, target: number): number => {
   const mathStr = cleanChromosome(chromosome)
@@ -160,29 +165,182 @@ const evaluateFitness = (chromosome: Chromosome, target: number): number => {
     // This is as far from the target number as it can get, so let's just say the fitness is 0.
     return 0;
   } else {
-    return 1 / Math.abs((target - n) + 1);
+    return 1 / (Math.abs(target - n) + 1);
   }
 };
 
-const chrom1: Chromosome = [
-  [0, 1, 1, 0], // 6
-  [0, 0], // +
-  [0, 1, 0, 1], // 5
-  [1, 0], // *
-  [0, 1, 0, 0], // 4
-  [1, 1], // /
-  [0, 0, 1, 0], // 2
-  [0, 1], // -
-  [1, 1, 1, 1], // (junk)
-  [0, 0], // +
-  [0, 0, 0, 1], // 1
-];
-const organism1: Organism = {
-  chromosome: chrom1,
-  fitness: 0
+// Step 2. Setting parameters
+
+// For running the algorithm, I'm going to make a main function whose arguments are the parameters to set. For this specific application, I need a couple extra parameters: the chromosome length for each organism and the target number.
+// This part is not linear. I'm going to fill in the steps of the algorithm as needed.
+const runAlgorithm = (
+  populationSize: number,
+  crossoverRate: number,
+  mutationRate: number,
+  generationLimit: number,
+  chromosomeLength: number,
+  target: number
+): void => {
+  console.log('Initializing population...');
+  
+  // Step 3. Create initial population
+  // Since fitness evaluation is to be done in batch, I'm going to create the population as chromosomes (without fitness).
+  const initialChromPopulation: Chromosome[] = buildArray(populationSize)
+    .map(_ => randomChromosome(chromosomeLength));
+
+  // Here's where the loop begins. One step at a time.
+  const generationalStep = (chromPopulation: Chromosome[], generation: number): Chromosome[] => {
+    console.log(`Generation: ${generation}`);
+    
+    // Check for the stopping condition.
+    if (generation >= generationLimit) {
+      return chromPopulation;
+    }
+
+    // Step 4. Fitness evaluation
+    const population: Organism[] = chromPopulation.map(chromosome => ({
+      chromosome,
+      fitness: evaluateFitness(chromosome, target)
+    }));
+
+    // Do Steps 5 to 7 (selection, crossover, and mutation) to produce two offspring.
+    // The offspring will be chromosomes instead of organisms, since their fitnesses won't be evaluated yet.
+    const reproduce = (): [Chromosome, Chromosome] => {
+      // Step 5. Selection
+      const parent1 = rouletteWheelSelection(population);
+      const parent2 = rouletteWheelSelection(population);
+
+      // Step 6. Crossover
+      const [chrom1, chrom2] = crossover(parent1.chromosome, parent2.chromosome, crossoverRate);
+
+      // Step 7. Mutation
+      const chrom1Mutated = mutate(chrom1, mutationRate);
+      const chrom2Mutated = mutate(chrom2, mutationRate);
+
+      return [chrom1Mutated, chrom2Mutated];
+    };
+
+    // Step 8. Replace population
+    const newChromPopulation: Chromosome[] = buildArray(Math.floor(populationSize / 2))
+      .map(reproduce)
+      .flat();
+
+    // Step 9. Repeat until the stopping condition is met
+    return generationalStep(newChromPopulation, generation + 1);
+  };
+
+  // Get the final population of chromosomes from the recursive generational steps.
+  const finalChromPopulation: Chromosome[] = generationalStep(initialChromPopulation, 0);
+
+  // Evaluate the fitness of each chromosome so I have a population of organisms.
+  const finalPopulation: Organism[] = finalChromPopulation.map(chromosome => ({
+    chromosome,
+    fitness: evaluateFitness(chromosome, target)
+  }));
+
+  // Step 10. Pick the winner
+  const winner = finalPopulation.reduce((best, organism) => organism.fitness > best.fitness ? organism : best);
+
+  // Print the winner's details.
+  console.log();
+  console.log(`The winner is...`);
+  console.log(`Phenotype: ${phenotype(winner)}`);
+  console.log(`Clean phenotype: ${cleanPhenotype(winner)}`);
+  console.log(`Result: ${evaluateMath(cleanPhenotype(winner))}`);
+  console.log(`Fitness: ${evaluateFitness(winner.chromosome, target)}`);
+
 };
-console.log('organism1');
-console.log('phenotype:', phenotype(organism1));
-console.log('cleanPhenotype:', cleanPhenotype(organism1));
-console.log('result:', evaluateMath(cleanPhenotype(organism1)));
-console.log('fitness:', evaluateFitness(organism1.chromosome, 42));
+
+// Create a randomized chromosome of a desired length.
+const randomChromosome = (length: number): Chromosome => {
+  const numberGenes: Gene[] = buildArray(Math.floor(length / 2))
+    .map(_ => randomGene("number"));
+  const operatorGenes: Gene[] = buildArray(Math.floor(length / 2 - 1))
+    .map(_ => randomGene("operator"));
+  return zip(numberGenes, operatorGenes)
+    .flat()
+    .concat([last(numberGenes)]);
+};
+
+type NumberOrOperator = "number" | "operator";
+
+// Create a randomized number or operator gene.
+const randomGene = (numberOrOperator: NumberOrOperator): Gene => {
+  switch (numberOrOperator) {
+    case "number":
+      return [randomBit(), randomBit(), randomBit(), randomBit()];
+    case "operator":
+      return [randomBit(), randomBit()];
+  }
+};
+
+// Generate a random bit (0 or 1).
+const randomBit = (): Bit => (Math.random() < 0.5) ? 0 : 1;
+
+// Select an organism from a population using the roulette wheel strategy.
+const rouletteWheelSelection = (population: Organism[]): Organism => {
+  const fitnesses: number[] = population.map(({fitness}) => fitness);
+  const totalFitness: number = sum(fitnesses);
+  const cumulFitnesses: number[] = scan((x: number, y: number) => x + y, fitnesses[0], fitnesses);
+  const withCumulativeFitnesses: [Organism, number][] = zip(population, cumulFitnesses);
+  const r: number = Math.random() * totalFitness;
+  const found: [Organism, number] | undefined = withCumulativeFitnesses.find(([_, cf]) => cf >= r);
+  if (found === undefined) {
+    // In case an organism is not found, return the last one.
+    return population[population.length - 1];
+  } else {
+    const [organism, _] = found;
+    return organism;
+  }
+};
+
+// Crossover (or clone) two chromosomes.
+const crossover = (x: Chromosome, y: Chromosome, crossoverRate: number): [Chromosome, Chromosome] => {
+  const r: number = Math.random();
+  if (r <= crossoverRate) {
+    const position: number = Math.floor(Math.random() * x.length);
+    const xNew = [...x.slice(0, position), ...y.slice(position)];
+    const yNew = [...y.slice(0, position), ...x.slice(position)];
+    return [xNew, yNew];
+  } else {
+    return [x, y];
+  }
+};
+
+// Flip a bit from 0 to 1, or 1 to 0.
+const flipBit = (bit: Bit): Bit => bit === 0 ? 1 : 0;
+
+// Mutate a chromosome.
+const mutate = (chromosome: Chromosome, mutationRate: number): Chromosome => {
+  // Mutate a bit.
+  const mutateBit = (bit: Bit): Bit => {
+    const r: number = Math.random();
+    return r <= mutationRate
+      ? flipBit(bit)
+      : bit;
+  };
+
+  // Mutate each gene.
+  return chromosome.map(gene => {
+    if (isNumber(gene)) {
+      const [a, b, c, d] = gene;
+      return [mutateBit(a), mutateBit(b), mutateBit(c), mutateBit(d)];
+    }
+
+    if (isOperator(gene)) {
+      const [a, b] = gene;
+      return [mutateBit(a), mutateBit(b)];
+    }
+
+    return gene;
+  });
+};
+
+// After playing with the parameters, I found that these values gave pretty consistently interesting results, and relatively fast.
+const populationSize = 200;
+const crossoverRate = 0.6;
+const mutationRate = 0.05;
+const generationLimit = 20;
+const chromosomeLength = 20;
+const target = 42;
+runAlgorithm(populationSize, crossoverRate, mutationRate, generationLimit, chromosomeLength, target);
--- typescript/src/util.ts
+++ typescript/src/util.ts
@@ -1,5 +1,6 @@
 // JavaScript/TypeScript's standard library is lacking in basic utility functions.
 
+// Check for array equality by comparing their values.
 export const eqArrays = (xs: any[], ys: any[]): boolean => {
   return (xs.length === ys.length) && xs.every((_, i) => {
     if (Array.isArray(xs[i]) && Array.isArray(ys[i])) {
@@ -10,15 +11,14 @@ export const eqArrays = (xs: any[], ys: any[]): boolean => {
   });
 };
 
+// Make a new array of a given size.
 export const buildArray = (size: number): undefined[] => [...Array(size)];
 
-export const range = (from: number, to: number): number[] => {
-  return [...Array(to - from + 1)].map((_, idx) => idx + from);
-};
+// Make an array of an element repeated n times.
+export const repeat = <A>(x: A, n: number): A[] => [...Array(n)].map(_ => x);
 
-export const repeat = <T>(x: T, n: number): T[] => [...Array(n)].map(_ => x);
-
-export const chunksOf = <T>(n: number, xs: T[]): T[][] => {
+// Split an array into chunks of a given size.
+export const chunksOf = <A>(n: number, xs: A[]): A[][] => {
   const size = Math.floor(xs.length / n);
   const initialChunks: number[][] = buildArray(size).map((_, idx) => repeat(idx, n));
   const extraChunks: number[] = repeat(initialChunks.length, xs.length % n);
@@ -29,4 +29,43 @@ export const chunksOf = <T>(n: number, xs: T[]): T[][] => {
     chunk.map((chunkNum, idx) =>
       (chunkNum * n) + idx));
   return indexes.map(chunk => chunk.map(idx => xs[idx]));
+};
+
+// Sum an array of numbers.
+export const sum = (ns: number[]): number => ns.reduce((acc, n) => acc + n, 0);
+
+// Zip two arrays together.
+// For example:
+//   [1, 2, 3, 4], ["one", "two", "three", "four"]
+//   -> [ [1, "one"], [2, "two"], [3, "three"], [4, "four"] ]
+export const zip = <A, B>(xs: A[], ys: B[]): [A, B][] => {
+  if (xs.length <= ys.length) {
+    return xs.reduce((acc: [A, B][], _, i) => (
+      [
+        ...acc,
+        [ xs[i], ys[i] ]
+      ]
+    ), []);
+  } else {
+    return ys.reduce((acc: [A, B][], _, i) => (
+      [
+        ...acc,
+        [ xs[i], ys[i] ]
+      ]
+    ), []);
+  }
+};
+
+// Returns the last element of the given list.
+export const last = <A>(xs: A[]): A => xs[xs.length - 1];
+
+// Scan is similar to reduce, but returns a list of successively reduced values from the left.
+export const scan = <A, B>(
+  fn: (acc: A, value: B) => A,
+  initial: A,
+  values: B[]
+): A[] => {
+  return values.reduce((acc, x) => {
+    return [...acc, fn(last(acc), x)];
+  }, [initial]);
 };
